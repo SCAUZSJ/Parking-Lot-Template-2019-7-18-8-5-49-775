c284e25b5efe9cd55d56c1a1036e6905bab242b6
SCAUZSJ <895104160@qq.com>
2019-07-18 19:22:34 +0800

    Add the method of add parkinglot in controller

--- src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
+++ src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
@@ -1,7 +1,11 @@
 package com.thoughtworks.parking_lot.controller;
 
+import com.thoughtworks.parking_lot.entity.ParkingLot;
 import com.thoughtworks.parking_lot.service.ParkingLotService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,14 @@ public class ParkingLotController {
 
     @Autowired
     private ParkingLotService parkingLotService;
+
+
+    @PostMapping(produces = {"application/json"})
+    public ResponseEntity add(@RequestBody ParkingLot parkingLot) {
+        if(!parkingLotService.add(parkingLot)){
+            return ResponseEntity.badRequest().build();
+        }
+        return  ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/com/thoughtworks/parking_lot/service/ParkingLotService.java
+++ src/main/java/com/thoughtworks/parking_lot/service/ParkingLotService.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.parking_lot.service;
 
+import com.thoughtworks.parking_lot.entity.ParkingLot;
 import com.thoughtworks.parking_lot.repository.ParkingLotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,15 @@ public class ParkingLotService {
 
     @Autowired
     private ParkingLotRepository parkingLotRepository;
+
+    public boolean add(ParkingLot parkingLot){
+        try {
+            parkingLotRepository.saveAndFlush(parkingLot);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return  false;
+        }
+    }
 
 }
